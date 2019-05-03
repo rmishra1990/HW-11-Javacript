976eca800e72526d7804b3aacb0ed5f51a00e08b
Ritu Mishra <rmishra1990@gmail.com>
2019-05-02 22:22:24 -0400

    Changes

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(tableData);
+  
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    
+
+
+});
